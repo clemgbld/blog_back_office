a230115b55fb524ef7d218d2242a1c02e3e5caee
cgombauld <clement.gombauld@cpexterne.org>
2022-10-12 08:20:37 +0200

    refacto scss to look like my portfolio

--- src/app/pages/CreateArticle/CreateArticle.tsx
+++ src/app/pages/CreateArticle/CreateArticle.tsx
@@ -16,12 +16,81 @@ const CreateArticle = () => {
 
   return (
     <div className={classNames["create-page"]}>
-      <h1 className={classNames["create-page_title"]}>Create new Article</h1>
+      <h1 className={classNames["create-page_title"]}>
+        <span className={classNames["create-page_title--three-d"]}>C</span>
+        reate new Article
+      </h1>
       <form>
-        <RichTextEditor
-          initialValue={articleContent}
-          onChange={handleValueChange}
-        />
+        <div className={classNames["create-page_flex-container"]}>
+          <div className={classNames["create-page_input-box"]}>
+            <label className={classNames["create-page_label"]} htmlFor="title">
+              Title:
+            </label>
+            <input
+              required
+              className={classNames["create-page_input"]}
+              id="title"
+              name="title"
+              type="text"
+            />
+          </div>
+          <div className={classNames["create-page_input-box"]}>
+            <label className={classNames["create-page_label"]} htmlFor="topic">
+              Topic:
+            </label>
+            <input
+              required
+              className={classNames["create-page_input"]}
+              id="topic"
+              name="topic"
+              type="text"
+            />
+          </div>
+        </div>
+        <div className={classNames["create-page_input-box"]}>
+          <label
+            className={classNames["create-page_label"]}
+            htmlFor="description"
+          >
+            Description:
+          </label>
+          <input
+            className={classNames["create-page_input"]}
+            id="description"
+            name="description"
+            type="text"
+          />
+        </div>
+        <div className={classNames["create-page_checkbox-container"]}>
+          <label htmlFor="description">Publish:</label>
+          <input
+            className={classNames["create-page_checkbox"]}
+            id="publish"
+            name="publish"
+            type="checkbox"
+          />
+        </div>
+
+        <div className={classNames["create-page_editor-container"]}>
+          <RichTextEditor
+            initialValue={articleContent}
+            onChange={handleValueChange}
+          />
+        </div>
+        <div className={classNames["create-page_button-container"]}>
+          <div className={classNames["create-page_button-flex"]}>
+            <button
+              className={`${classNames["create-page_button"]} ${classNames["create-page_button--cancel"]}`}
+            >
+              Cancel
+            </button>
+            <button
+              className={`${classNames["create-page_button"]} ${classNames["create-page_button--valid"]}`}
+            >
+              Save
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
