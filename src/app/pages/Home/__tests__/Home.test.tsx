4d4e5d2d65cd3b738992d5e630453779bf666b2d
cgombauld <clement.gombauld@cpexterne.org>
2022-10-18 09:42:45 +0200

    setup test for home page

--- src/app/pages/Home/__tests__/Home.test.tsx
+++ src/app/pages/Home/__tests__/Home.test.tsx
@@ -1,8 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { createStore } from "../../../../core/store";
+import { inMemoryArticlesService } from "../../../../core/articles/infrastructure/in-memory-services/InMemoryArticlesService";
+import {
+  fakeArticle1,
+  fakeArticle2,
+} from "../../../articles/fixtures/articles";
 import Home from "../Home";
 
 describe("Home", () => {
-  it("should sucessfully fecth articles", () => {});
+  it("should sucessfully fecth articles", () => {
+    const store = createStore({
+      services: {
+        articlesService: inMemoryArticlesService([fakeArticle1, fakeArticle2]),
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+  });
 });
