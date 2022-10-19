901316907403f36528275c6a019b6188b3732f31
cgombauld <clement.gombauld@cpexterne.org>
2022-10-19 07:43:59 +0200

    refacto article card in its own component

--- src/app/articles/ArticleCard/ArticleCard.tsx
+++ src/app/articles/ArticleCard/ArticleCard.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { FC } from "react";
+import { selectFirstImg } from "../../../core/articles/selectors/select-first-img/select-first-img";
 
-const ArticleCard = () => {
-  return <div>ArticleCard</div>;
+type ArticleCardProps = {
+  title: string;
+  summary?: string;
+  timeToRead: string;
+  date: string;
+  content: any;
+  topic?: string;
+};
+
+const ArticleCard: FC<ArticleCardProps> = ({
+  title,
+  summary,
+  timeToRead,
+  date,
+  content,
+  topic,
+}) => {
+  const { src, alt } = selectFirstImg(content);
+
+  return (
+    <figure>
+      <div>
+        <img src={src} alt={alt} />
+      </div>
+      <figcaption>
+        <h2>{title}</h2>
+        <div>
+          <span>{date}</span>
+          <span>{timeToRead}</span>
+          <span>{topic}</span>
+        </div>
+        <p>{summary}</p>
+      </figcaption>
+    </figure>
+  );
 };
 
 export default ArticleCard;
