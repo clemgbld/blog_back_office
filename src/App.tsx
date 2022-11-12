611905eed2c7ba110a7eb5b3679f4ab336331bdd
cgombauld <clement.gombauld@cpexterne.org>
2022-11-12 17:55:28 +0100

    private routing implemented on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,57 @@
-import Home from "./app/pages/Home/Home";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "./core/auth/selectors/selectors";
+
 import Header from "./app/UI/Header/Header";
+import Home from "./app/pages/Home/Home";
 import CreateArticle from "./app/pages/CreateArticle/CreateArticle";
 import UpdateArticle from "./app/pages/UpdateArticle/UpdateArticle";
+import Auth from "./app/pages/Auth/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./app/routing/ProtectedRoute/ProtectedRoute";
 import { ROUTES } from "./app/routing/constants";
 
 function App() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route
+          path={ROUTES.AUTH}
+          element={
+            <ProtectedRoute isAllowed={!isLoggedIn} redirectPath={ROUTES.HOME}>
+              <Auth />
+            </ProtectedRoute>
+          }
+        />
+        <Route
           path={ROUTES.HOME}
           element={
-            <Header>
-              <Home />
-            </Header>
+            <ProtectedRoute isAllowed={isLoggedIn} redirectPath={ROUTES.AUTH}>
+              <Header>
+                <Home />
+              </Header>
+            </ProtectedRoute>
           }
         />
         <Route
           path={ROUTES.CREATE}
           element={
-            <Header>
-              <CreateArticle />
-            </Header>
+            <ProtectedRoute isAllowed={isLoggedIn} redirectPath={ROUTES.AUTH}>
+              <Header>
+                <CreateArticle />
+              </Header>
+            </ProtectedRoute>
           }
         />
         <Route
           path={`${ROUTES.UPDATE}/:id`}
           element={
-            <Header>
-              <UpdateArticle />
-            </Header>
+            <ProtectedRoute isAllowed={isLoggedIn} redirectPath={ROUTES.AUTH}>
+              <Header>
+                <UpdateArticle />
+              </Header>
+            </ProtectedRoute>
           }
         />
       </Routes>
