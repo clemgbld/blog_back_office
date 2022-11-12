429e0b95978ce755f29cbb365d1264af88fffb4e
cgombauld <clement.gombauld@cpexterne.org>
2022-11-12 13:15:35 +0100

    implemented loginFromStorage in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,10 @@ import { createClock } from "./core/infastructure/create-clock";
 import { createStore } from "./core/store";
 import { ClockContext } from "./app/context/ClockContext";
 import { fakeArticle1, fakeArticle2 } from "./app/articles/fixtures/articles";
+import { loginFromStorage } from "./core/auth/use-cases/login-from-storage";
 
 const error =
-  process.env.REACT_APP_ARG === "error"
+  process.env.REACT_APP_ARG === "inMemory error"
     ? {
         status: 404,
         message: "Something went wrong",
@@ -31,15 +32,18 @@ export type RootState = ReturnType<typeof store.getState>;
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <ClockContext.Provider value={createClock.create()}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ClockContext.Provider>
-  </Provider>
-);
+
+store.dispatch(loginFromStorage()).then(() => {
+  root.render(
+    <Provider store={store}>
+      <ClockContext.Provider value={createClock.create()}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ClockContext.Provider>
+    </Provider>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
