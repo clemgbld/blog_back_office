a2188469f9f4da948f352f25c026e1923c13df8a
cgombauld <clement.gombauld@cpexterne.org>
2022-12-11 16:33:23 +0100

    fixed form

--- src/app/pages/UpdateArticle/UpdateArticle.tsx
+++ src/app/pages/UpdateArticle/UpdateArticle.tsx
@@ -64,7 +64,7 @@ const UpdateArticle = () => {
         title: inputValues.title,
         summary: inputValues.description,
         topic: inputValues.topic,
-        hide: inputValues.hide,
+        hide: !inputValues.hide,
         date: articleToUpdate?.date || 0,
         lightMode: isEditorInLightMode,
         content,
