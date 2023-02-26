bc4654b2b4616d81fc73e15693963c320efa2345
cgombauld <clement.gombauld@cpexterne.org>
2023-02-26 16:01:05 +0100

    refacto

--- src/core/articles/articles-slice.ts
+++ src/core/articles/articles-slice.ts
@@ -32,12 +32,32 @@ type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>;
 
 type PendingAction = ReturnType<GenericAsyncThunk["pending"]>;
 
+type FulfilledAction = ReturnType<GenericAsyncThunk["fulfilled"]>;
+
 function isPendingAction(action: AnyAction): action is PendingAction {
   return [
     updateArticle.pending.type,
     retrieveArticles.pending.type,
     deleteArticle.pending.type,
     postArticle.pending.type,
+  ].includes(action.type);
+}
+
+function isFulfilledAction(action: AnyAction): action is FulfilledAction {
+  return [
+    updateArticle.fulfilled.type,
+    retrieveArticles.fulfilled.type,
+    deleteArticle.fulfilled.type,
+    postArticle.fulfilled.type,
+  ].includes(action.type);
+}
+
+function isRejectedAction(action: any) {
+  return [
+    updateArticle.rejected.type,
+    retrieveArticles.rejected.type,
+    deleteArticle.rejected.type,
+    postArticle.rejected.type,
   ].includes(action.type);
 }
 
@@ -52,44 +72,31 @@ export const articlesSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(retrieveArticles.fulfilled, (state, action) => {
-        state.status = STATUS.SUCCESS;
         state.isArticlesRetrieved = true;
         articlesAdapter.setAll(state.data, action.payload);
       })
 
-      .addCase(retrieveArticles.rejected, (state, action) => {
-        state.status = STATUS.REJECTED;
-        state.error = action.error.message;
-      })
       .addCase(postArticle.fulfilled, (state, action) => {
-        state.status = STATUS.SUCCESS;
         articlesAdapter.addOne(state.data, action.payload);
       })
 
-      .addCase(postArticle.rejected, (state, action) => {
-        state.status = STATUS.REJECTED;
-        state.error = action.error.message;
-      })
       .addCase(updateArticle.fulfilled, (state, action) => {
-        state.status = STATUS.SUCCESS;
         articlesAdapter.setOne(state.data, action.payload);
       })
 
-      .addCase(updateArticle.rejected, (state, action) => {
-        state.status = STATUS.REJECTED;
-        state.error = action.error.message;
-      })
       .addCase(deleteArticle.fulfilled, (state, action) => {
-        state.status = STATUS.SUCCESS;
         articlesAdapter.removeOne(state.data, action.payload);
       })
 
-      .addCase(deleteArticle.rejected, (state, action) => {
-        state.status = STATUS.REJECTED;
-        state.error = action.error.message;
-      })
       .addMatcher(isPendingAction, (state, action) => {
         state.status = STATUS.PENDING;
+      })
+      .addMatcher(isFulfilledAction, (state, action) => {
+        state.status = STATUS.SUCCESS;
+      })
+      .addMatcher(isRejectedAction, (state, action) => {
+        state.status = STATUS.REJECTED;
+        state.error = action.error.message;
       });
   },
 });
