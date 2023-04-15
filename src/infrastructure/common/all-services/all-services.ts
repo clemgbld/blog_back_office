c33adcd528c7ac77db1b24bc8bef19a005a2736a
cgombauld <clement.gombauld@cpexterne.org>
2023-04-15 16:39:43 +0200

    replace inMemory by rest services

--- src/infrastructure/common/all-services/all-services.ts
+++ src/infrastructure/common/all-services/all-services.ts
@@ -3,14 +3,14 @@ import { createClock } from "../create-clock";
 import { buildRestArticlesService } from "../../articles/rest-services/articles-service";
 import { buildRestAuthService } from "../../auth/rest-services/rest-auth-service";
 import { buildInMemoryServices } from "./all-services-in-memory";
-import { buildInMemoryEmailNotificationService } from "../../articles/in-memory-services/in-memory-email-notification-service";
-import { buildInMemorySubscriptionService } from "../../emails/in-memory-services/in-memory-subscription-service";
+import { buildRestEmailNotificationService } from "../../articles/rest-services/email-notification-service";
+import { buildRestSubscriptionService } from "../../emails/rest-services/subscription-service";
 
 export const buildServices = (): ReturnType<typeof buildInMemoryServices> => ({
   storageService: createStorageService(localStorage),
   clockService: createClock.create(),
   authService: buildRestAuthService(),
   articlesService: buildRestArticlesService(),
-  emailNotificationService: buildInMemoryEmailNotificationService(),
-  subscriptionService: buildInMemorySubscriptionService({ emails: [] }),
+  emailNotificationService: buildRestEmailNotificationService(),
+  subscriptionService: buildRestSubscriptionService(),
 });
